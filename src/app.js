fcfea749911316353b1bc475c9032285934b05e2
Kevin Ocampo <161964924+Ocampo81@users.noreply.github.com>
2024-04-18 19:37:17 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,24 @@
-/* eslint-disable */
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adjective = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+
+  let textOf = `<ul class="list-group list-group-flush">
+  <li class="list-group-item bg-dark text-white">Domain names generados</li>`;
+
+  for (const iterator of pronoun) {
+    for (const item of adjective) {
+      for (const element of noun) {
+        textOf += `
+          <li class="list-group-item">
+            ${iterator}${item}${element}.com
+          </li>`;
+      }
+    }
+  }
+  textOf += "</ul>";
+  document.querySelector("#for").innerHTML = textOf;
 };
